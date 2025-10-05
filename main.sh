e76a4967aa7af680e546f668a81a33620c445588
Andrey Oliveira <andreyprado05@gmail.com>
2025-10-05 19:12:02 -0300

    Começo da criação de usuário

--- main.sh
+++ main.sh
@@ -1,1 +1,13 @@
-!# 
+#!/bin/bash
+
+dnf update
+
+read -r -p "Quer criar um usuário para o sistema (S/n)? " $resposta
+
+while [ "$resposta" = "S" ];
+do
+	read -r -p "Digite o nome do novo usuário: " $nomeUser
+	useradd $nomeUser
+
+	
+done
