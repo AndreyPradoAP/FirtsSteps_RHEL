ba753f9f496c7b7a4802073f047a0e0589ba5398
Andrey Oliveira <andreyprado05@gmail.com>
2025-10-05 20:36:43 -0300

    Criação de usuário OK (Melhorar no futuro)

--- main.sh
+++ main.sh
@@ -2,12 +2,17 @@
 
 dnf update
 
-read -r -p "Quer criar um usuário para o sistema (S/n)? " $resposta
+read -r -p "Quer criar um usuário para o sistema (S/n)? " resposta
 
 while [ "$resposta" = "S" ];
 do
-	read -r -p "Digite o nome do novo usuário: " $nomeUser
+	# Criação de usuário sudoers
+	read -r -p "Digite o nome do novo usuário: " nomeUser
 	useradd $nomeUser
+	# Adicionar uma forma de verificar se o usuário foi criado
+	echo "Usuário criado. Digite uma senha para ele"
+	passwd $nomeUser
+	usermod -aG wheel $nomeUser
 
-	
+	read -r -p "Criar um novo usuário (S/n)? " resposta
 done
